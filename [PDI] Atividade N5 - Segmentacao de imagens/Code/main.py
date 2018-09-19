6d4b26abb03c9e20aa5a0e3f6121bd28751511f3
Aryell Dias <aryelld@gmail.com>
2018-09-19 18:25:19 -0300

    ajustes

--- [PDI] Atividade N5 - Segmentacao de imagens/Code/main.py
+++ [PDI] Atividade N5 - Segmentacao de imagens/Code/main.py
@@ -3,23 +3,21 @@ import matplotlib.pyplot as plt
 import utils
 import filters
 import bordas
-
+import segmentacao
 
 def main(imagem,tipo,filtro,m,n):
-    ImgOriginal = utils.LerImagem('../imagens/{}.jpg'.format(imagem))    
-    
-    ImgFiltrada = bordas.Derivative(ImgOriginal,tipo, filtro,m,n)
-    
-    BinaryImg = utils.Threshold(ImgFiltrada)    
+    ImgOriginal = utils.LerImagem('../images/{}.jpg'.format(imagem))    
+        
+    BinaryImg = segmentacao.otsu(ImgOriginal)    
 
     fig, [ax1,ax2] = plt.subplots(1,2,figsize=(20,30))
     ax1.imshow(ImgOriginal,cmap='gray')
     ax2.imshow(BinaryImg,cmap='gray')
     
-    plt.savefig('../resultados/teste_{}_{}_{}_{}x{}.png'.format(tipo,imagem,filtro,m,n),dpi=150,bbox_inches='tight')
+    # plt.savefig('../resultados/teste_{}_{}_{}_{}x{}.png'.format(tipo,imagem,filtro,m,n),dpi=150,bbox_inches='tight')
     
     plt.show()
     
 
 if __name__ == '__main__':
-    main('Image_(1)','Sobel','Average',5,5)
+    main('Image_(5)','Sobel','Average',5,5)
