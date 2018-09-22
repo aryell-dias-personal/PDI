ac988e1dad7445ccafb1f13de6b6483efa83be98
Aryell Dias <aryelld@gmail.com>
2018-09-22 10:52:09 -0300

    Questão 1 aparentemente feita

--- [PDI] Atividade N6 - Morfologia/Code/main.py
+++ [PDI] Atividade N6 - Morfologia/Code/main.py
@@ -2,17 +2,18 @@ import matplotlib.pyplot as plt
 import utils
 
 
-def mainOpera(imagem1,imagem2,Operacao):
-    ImgOriginal1 = utils.LerImagem('../imagens/originais/{}.jpg'.format(imagem1))
-    ImgOriginal2 = utils.LerImagem('../imagens/originais/{}.jpg'.format(imagem2))   
+def mainOpera(imagem1,imagem2,operacao):
+    # formato das imagens 1a e 1b são diferentes do esperado 
+    ImgOriginal1 = utils.LerImagem('../Imagens/Originais/{}.png'.format(imagem1))
+    ImgOriginal2 = utils.LerImagem('../Imagens/Originais/{}.png'.format(imagem2))   
 
-    # TODO something
+    ImgResultado = utils.operation(ImgOriginal1,ImgOriginal2,operacao)
 
     fig, [ax1,ax2,ax3] = plt.subplots(1,3,figsize=(20,30))
     ax1.imshow(ImgOriginal1,cmap='gray')
-    ax2.imshow(ImgOriginal1,cmap='Greys')
-    ax2.imshow("",cmap='Greys')
+    ax2.imshow(ImgOriginal2,cmap='gray')
+    ax3.imshow(ImgResultado,cmap='gray')
     plt.show()
     
 if __name__ == '__main__':
-    mainOpera('Image_(1a)','Image_(1b)','And')
+    mainOpera('Image_(1a)','Image_(1b)','xand')
--- [PDI] Atividade N6 - Morfologia/Code/utils.py
+++ [PDI] Atividade N6 - Morfologia/Code/utils.py
@@ -6,3 +6,21 @@ def LerImagem(nome):
     imagem = mpimg.imread(nome)
     return imagem
 
+# as operações foram interpretadas como sendo as classicas
+# utilizando dos pixels nas correspondentes de uma imagem 
+# para outr
+
+def operation(img1,img2, operation):
+    img1 = np.array(img1[:,:,0], dtype=bool)
+    img2 = np.array(img2[:,:,0], dtype=bool)
+    x,y = np.shape(img1)
+    if(operation == 'and'):
+        return [[img1[i][j] and img2[i][j] for j in range(y)]for i in range(x)]
+    elif(operation == 'or'):
+        return [[img1[i][j] or img2[i][j] for j in range(y)]for i in range(x)]
+    elif(operation == 'xor'):
+        return [[((not img1[i][j]) and (img2[i][j])) or ((img1[i][j]) and (not img2[i][j])) for j in range(y)]for i in range(x)]
+    # seria um xnor?
+    elif(operation == 'xand'):
+        return [[((not img1[i][j]) and (not img2[i][j])) or ((img1[i][j]) and (img2[i][j])) for j in range(y)]for i in range(x)]
+        
