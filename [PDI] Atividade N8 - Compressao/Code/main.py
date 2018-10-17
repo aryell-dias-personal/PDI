5ca8a664896f552b629654b1994d894847d1f4b3
rodrigomelo89 <rodrigo.melo89@yahoo.com.br>
2018-10-17 05:12:24 -0300

    commit inicial atividade 9

--- [PDI] Atividade N8 - Compressao/Code/main.py
+++ [PDI] Atividade N8 - Compressao/Code/main.py
@@ -19,22 +19,23 @@ def mainQuant(imagem,bit):
 
 if __name__ == '__main__':
     # mainQuant('Image_(3)',8)
-    ImgOriginal = utils.LerImage('Image_(2)')
-    # ImgOriginal = [[39,39,126,126],[39,39,126,126],[39,39,126,126],[39,39,126,126]]
+    # ImgOriginal = utils.LerImage('Image_(2)')
+    ImgOriginal = [[39,39,126,126],[39,39,126,126],[39,39,126,126],[39,39,126,126]]
 
-    q = np.shape(ImgOriginal)
+    # q = np.shape(ImgOriginal)
 
     # q = compressao.Huffman('Image_(1)')
     # q = utils.Pad(ImgOriginal,8,8)
     dictionary = compressao.LZW(ImgOriginal)
     compress = compressao.LZWcompress(dictionary)
     decompress = descompressao.LZWdescompress(dictionary,compress,(4,4))
-    print(decompress)
+    decompress = np.uint8(decompress)
+    # print(decompress, np.shape(decompress))
 
     # q = compressao.LZW(ImgOriginal,8192)    
-    # fig,[ax1, ax2] = plt.subplots(1,2)
-    # ax1.imshow(ImgOriginal, cmap='gray')
-    # ax2.imshow(q,cmap='gray')
-    # plt.show()
+    fig,[ax1, ax2] = plt.subplots(1,2)
+    ax1.imshow(ImgOriginal, cmap='gray')
+    ax2.imshow(decompress,cmap='gray')
+    plt.show()
 
     
---
+++ [PDI] Atividade N9 - Imagens coloridas/Code/main.py
@@ -0,0 +1,5 @@
+import matplotlib.pyplot as plt 
+import numpy as np
+
+
+if __name__ == '__main__':
