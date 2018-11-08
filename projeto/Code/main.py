ac00f955deb183ee5c7042b4e7bbd39ba3684d88
rodrigomelo89 <rodrigo.melo89@yahoo.com.br>
2018-11-08 09:34:38 -0300

    re-organização

--- projeto/Code/main.py
+++ projeto/Code/main.py
@@ -1,3 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt 
+import utils
 
+if __name__ == '__main__':
+    img = utils.LerImage('63')
+
+    imagem = utils.rgb2gray(img)
+
+    imagem = utils.Median(imagem)
+
+    imagem = utils.Binarizar(imagem)
+
+    plt.imshow(imagem,cmap='gray')
+    plt.show()
--- projeto/Code/utils.py
+++ projeto/Code/utils.py
@@ -2,7 +2,11 @@ import numpy as np
 import matplotlib.image as mpimg
 
 def LerImage(imagem):
-    img = mpimg.imread('../imagens/{}.jpg'.format(imagem))
+    try:
+        img = mpimg.imread('../imagens/{}.jpg'.format(imagem))
+    except:
+        img = mpimg.imread('../imagens/{}.png'.format(imagem))
+
     return img
 
 def rgb2gray(imagem):
@@ -10,3 +14,55 @@ def rgb2gray(imagem):
 
     return img
 
+def Median(imagem,m=5,n=5):
+    aux = np.shape(imagem)
+
+    if np.size(aux) > 2:
+        imagem = imagem[:][:][0]
+        aux = np.shape(imagem)
+    
+    filterImg = np.zeros(aux)
+    kernel = np.zeros(m*n)
+
+    for x in range(aux[0]):
+        for y in range(aux[1]):
+            for u in range(m*n):
+                if (x+u%m-1 >= 0) and (x+u%m-1 < aux[0]) and (y+u%n-1 >= 0) and (y+u%n-1 < aux[1]):
+                    kernel[u] = imagem[x+u%m-1][y+u%n-1]
+            for v in range(m*n):
+                for t in range(v,m*n):
+                    if kernel[v] > kernel[t]:
+                        auxi = kernel[t]
+                        kernel[t] = kernel[v]
+                        kernel[v] = auxi
+            if (m*n)%2 > 0:
+                filterImg[x][y] = kernel[int((m*n-1)/2)]
+            else:
+                filterImg[x][y] = (kernel[int(np.floor(m*n/2))]+kernel[int(np.ceil(m*n/2))])/2
+
+    # filterImg = imagem - filterImg
+
+    # plt.imshow(filterImg,cmap='gray')
+    # plt.show()
+
+    return filterImg
+
+def Binarizar(imagem):
+    aux = np.shape(imagem)
+
+    if np.size(aux) > 2: # seleciona apenas uma matriz de cor caso a leitura seja rgb
+        imagem = imagem[:,:,0]
+        aux = np.shape(imagem)
+
+    # imagem binária, 0 se for menor do que 128 e 1 maior do que 128
+
+    ImgBin = np.zeros(aux)
+    
+    for x in range(aux[0]):
+        for y in range(aux[1]):
+            if imagem[x][y] >= 0.4*np.max(imagem):
+                ImgBin[x][y] = 1
+            else:
+                ImgBin[x][y] = 0
+
+    return ImgBin
